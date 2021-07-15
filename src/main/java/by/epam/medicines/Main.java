f18723c4e96aa656b25ccb2ad7a5d4663d75bc88
realovka <realovka@mail.ru>
2021-07-15 09:00:36 +0300

    refactor xml-file

--- src/main/java/by/epam/medicines/Main.java
+++ src/main/java/by/epam/medicines/Main.java
@@ -6,21 +6,21 @@ import by.epam.medicines.validator.MedicineXmlValidator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Main {
+public class Main{
     private static final Logger logger = LogManager.getLogger();
-    private static final String RELATIVE_FILE_PATH = "E:\\epam\\medicines\\src\\main\\resources\\data\\medicines.xml";
-    private static final String RELATIVE_SCHEMA_PATH = "vouchers.xsd";
+    private static final String RELATIVE_FILE_PATH = "/data/medicines.xml";
+    private static final String RELATIVE_SCHEMA_PATH = "/data/medicines.xsd";
 
     public static void main(String[] args) throws MedicineException {
 
-//        MedicineXmlValidator validator = new MedicineXmlValidator();
-//        if (!validator.isValidXmlFile(RELATIVE_FILE_PATH, RELATIVE_SCHEMA_PATH)) {
-//            return;
-//        }
+        MedicineXmlValidator validator = new MedicineXmlValidator();
+        if (!validator.isValidXmlFile(RELATIVE_FILE_PATH, RELATIVE_SCHEMA_PATH)) {
+            return;
+        }
 
-        AbstractMedicineBuilder saxBuilder = MedicineBuilderFactory.createBuilder("sax");
-        saxBuilder.buildMedicines(RELATIVE_FILE_PATH);
-        System.out.println(saxBuilder.getMedicines());
+//        AbstractMedicineBuilder saxBuilder = MedicineBuilderFactory.createBuilder("sax");
+//        saxBuilder.buildMedicines(RELATIVE_FILE_PATH);
+//        System.out.println(saxBuilder.getMedicines());
 
     }
 }
---
+++ src/test/java/by/epam/medicines/validator/MedicineXmlValidatorTest.java
@@ -0,0 +1,17 @@
+package by.epam.medicines.validator;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class MedicineXmlValidatorTest {
+
+    @BeforeMethod
+    public void setUp() {
+    }
+
+    @Test
+    public void testIsValidXmlFile() {
+    }
+}
